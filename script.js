723aeaaba8a87e1b64297a05d4fffa40bb3c356b
David Earle <djearle98@icloud.com>
2020-04-22 01:20:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,94 @@
-class node {
+/*CLASSES*/
+class Scene {
+	constructor () {
+  	this.html = "";
+    this.applyCss = null;
+  }
+  paint(){
+		canvas.innerHTML = this.html;
+    this.applyCss(); 
+  }
+}
+class SingleNodeScene extends Scene {
+	constructor () {
+  	super();
+  }
+  paint(nodeId){
+    /*
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname">
+		*/
+    var node = allNodes.get(nodeId);
+    this.html = "\
+    	<div class='node'> \
+      	<label for='title' style='display:none'>Title:</label> \
+				<input disabled type='text' class='title' name='title' value='"+node.title+"'> \
+      	<div class='line-item'> \
+          <div class='label'>Description:</div> \
+          <div class='data'>"+node.description+"</div> \
+				</div> \
+      	<div class='line-item'> \
+          <div class='label'>Owner:</div> \
+          <div class='data'>"+node.owner+"</div> \
+				</div> \
+      	<div class='line-item'> \
+          <div class='label'>Deadline:</div> \
+          <div class='data'>"+node.deadline+"</div> \
+				</div> \
+      	<div class='line-item'> \
+          <div class='label'>Staus:</div> \
+          <div class='data'>"+node.status+"</div> \
+				</div> \
+			</div>";
+		
+    
+    super.applyCss = function(){
+      //canvas.getElementsByClassName("node")[0].style.backgroundColor = "blue";
+    };
+    super.paint();
+  }
+}
+class NetworkNodeScene extends Scene {
+	constructor () {
+  	super();
+  }
+  paint(){
+    /*
+    <label for="fname">First name:</label>
+    <input type="text" id="fname" name="fname">
+		*/
+    for (var i = 0; i< allNodes.size; i++){
+      var node = allNodes.get(i+1);
+      this.html += "\
+        <div class='node' onclick='singleNodeScene.paint("+node.nodeId+")'> \
+          <label for='title' style='display:none'>Title:</label> \
+          <input disabled type='text' class='title' name='title' value='"+node.title+"'> \
+          <div class='line-item'> \
+            <div class='label'>Description:</div> \
+            <div class='data'>"+node.description+"</div> \
+          </div> \
+          <div class='line-item'> \
+            <div class='label'>Owner:</div> \
+            <div class='data'>"+node.owner+"</div> \
+          </div> \
+          <div class='line-item'> \
+            <div class='label'>Deadline:</div> \
+            <div class='data'>"+node.deadline+"</div> \
+          </div> \
+          <div class='line-item'> \
+            <div class='label'>Staus:</div> \
+            <div class='data'>"+node.status+"</div> \
+          </div> \
+        </div>";
+    }
+    
+    super.applyCss = function(){
+      canvas.getElementsByClassName("node")[0].style.backgroundColor = "blue";
+    };
+    super.paint();
+  }
+}
+class Node {
 	constructor (nodeId, title, description, owner, deadline, status, parents, children) {
 		this.nodeId = nodeId;
 		this.title = title;
@@ -8,39 +98,18 @@ class node {
     this.status = status;
     this.parents = parents;
     this.children = children; 
-    
-    /*
-    <label for="fname">First name:</label>
-    <input type="text" id="fname" name="fname">
-		*/
-    document.getElementById("NodeNetwork").innerHTML += "\
-    	<div class='node'> \
-      	<label for='title' style='display:none'>Title:</label> \
-				<input disabled type='text' class='title' name='title' value='"+this.title+"'> \
-      	<div class='line-item'> \
-          <div class='label'>Description:</div> \
-          <div class='data'>"+this.description+"</div> \
-				</div> \
-      	<div class='line-item'> \
-          <div class='label'>Owner:</div> \
-          <div class='data'>"+this.owner+"</div> \
-				</div> \
-      	<div class='line-item'> \
-          <div class='label'>Deadline:</div> \
-          <div class='data'>"+this.deadline+"</div> \
-				</div> \
-      	<div class='line-item'> \
-          <div class='label'>Staus:</div> \
-          <div class='data'>"+this.status+"</div> \
-				</div> \
-			</div>";
   }
 }
 
+/*VARIABLES*/
+var canvas = document.getElementById("canvas");
 var allNodes = new Map();
-//initialize nodes
-for (var i = 1; i<=9; i++) {
-	allNodes.set(i, new node(i, "Node " + i,"This is Node " + i, "David", "12/12/2020", "Great", [], []));
-  console.log(allNodes.get(i));
-}
+var singleNodeScene = new SingleNodeScene();
+var networkNodeScene = new NetworkNodeScene();
 
+/*INIT*/
+for (var i = 1; i<=9; i++) {
+	allNodes.set(i, new Node(i, "Node " + i,"This is Node " + i, "David", "12/12/2020", "Great", [], []));
+  //console.log(allNodes.get(i));
+}
+networkNodeScene.paint();
